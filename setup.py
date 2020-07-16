9331c5696b849f1e4eb1ea047b5f9a7ce1b241c2
oczoske <ollicz68@yahoo.com>
2020-07-16 13:29:55 +0200

    v0.3, release candidate 1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from io import open     # in py3 just an alias to builtin 'open'.
 # Version number
 MAJOR = 0
 MINOR = 3
-ATTR = 'dev'
+ATTR = 'rc1'
 VERSION = '%d.%d%s' % (MAJOR, MINOR, ATTR)
 
 
